d0b00302a5ca99b82613c1a37e44ccaba94a4eee
unknown <zahra.dhanerawala@gmail.com>
2023-01-23 11:52:08 -0600

    suite2p concat

--- concat_sessions_for_suite2p.py
+++ concat_sessions_for_suite2p.py
@@ -5,12 +5,17 @@ Created on Mon Jan 23 10:47:19 2023
 @author: Han
 """
 
-import os, numpy as np, tifffile as tif
+import os, numpy as np, tifffile as tif, shutil
 
 dr = r'H:\imaging_data'
-concattifs = [] #in order of days?
+concattifs = [] #in order of days
+concattifnm = [] #also names of days
 days = os.listdir(dr); days.sort()
-for day in :
-    daydr = os.path.join(dr, daydr)
+dst = r'H:\imaging_data\concat'
+for day in days:
+    print(day)
+    daydr = os.path.join(dr, day)
     # find images from session, split up in 3000 frame tifs
-    tifs = [os.path.join(daydr, tif) for tifs in os.listdir(daydr) if '.tif' in tif]
+    tifs = [os.path.join(daydr, tiff) for tiff in os.listdir(daydr) if '.tif' in tiff]; tifs.sort()
+    [shutil.copy(tiff, dst) for tiff in tifs]
+    tifs = None
