203dca1ababe4abf4f3d8ed4d3ae27be3a31696f
unknown <zahra.dhanerawala@gmail.com>
2023-01-31 16:01:55 -0600

    validating results across 20 days

--- move_fmats.py
+++ move_fmats.py
@@ -11,6 +11,6 @@ dst = r"Z:\cellreg1month_Fmats"
 # move all converted fmats to separate folder
 for i in os.listdir(src):
     pth = os.path.join(src, i)
-    mat = os.path.join(pth, "suite2p", "plane0", "converted_Fall.mat")
+    mat = os.path.join(pth, "suite2p", "plane0", "Fall.mat")
     if os.path.exists(mat):
-        shutil.move(mat, os.path.join(dst, i+"_converted_Fall.mat"))
+        shutil.move(mat, os.path.join(dst, i+"_Fall.mat"))
