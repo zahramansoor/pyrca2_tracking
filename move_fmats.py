0036e5645f0d2187b0197e31be9cbfb8819bf824
Zahra D <zahra.dhanerawala@gmail.com>
2023-03-02 09:51:44 -0600

    move fmat edits for new imaging structure

--- move_fmats.py
+++ move_fmats.py
@@ -6,11 +6,18 @@ Created on Mon Jan 30 10:59:23 2023
 """
 
 import os, shutil
-src = r"Z:\imaging_yc"
-dst = r"Z:\cellreg1month_Fmats"
+src = r"Z:\sstcre_imaging\e201"
+dst = r"Z:\sstcre_analysis"
 # move all converted fmats to separate folder
 for i in os.listdir(src):
-    pth = os.path.join(src, i)
-    mat = os.path.join(pth, "suite2p", "plane0", "Fall.mat")
+    pth = os.path.join(src, str(i))
+    if "week" not in i: #week folders don't have sbx file
+        imgfl = [os.path.join(pth, xx) for xx in os.listdir(pth) if "000" in xx][0]
+    else:
+        imgfl = pth
+    mat = os.path.join(imgfl, "suite2p", "plane0", "Fall.mat") 
     if os.path.exists(mat):
-        shutil.move(mat, os.path.join(dst, i+"_Fall.mat"))
+        try:
+            shutil.copy(mat, os.path.join(dst, f"e200_day{int(i):03d}_Fall.mat"))
+        except: # for weeks
+            shutil.copy(mat, os.path.join(dst, f"e200_week{int(i[4:]):02d}_Fall.mat"))
